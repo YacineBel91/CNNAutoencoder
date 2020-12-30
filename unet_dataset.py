cc6ec21fbf085567efa71f025881fef418a3367a
SteampunkIslande <charles.monod-broca@neuf.fr>
2020-12-30 16:57:11 +0100

    Minor auto-reformat python code

--- unet_dataset.py
+++ unet_dataset.py
@@ -1,17 +1,16 @@
 import os
 from random import shuffle
 
-import torchvision
-
-from torch.utils.data import Dataset
-import rawpy
-
 import numpy as np
+import rawpy
+import torchvision
+from torch.utils.data import Dataset
 
-def pack_raw(raw,bps=14):
+
+def pack_raw(raw, bps=14):
     # pack Bayer image to 4 channels
     im = raw.raw_image_visible.astype(np.float32)
-    im = np.maximum(im - 512, 0) / ((2**bps-1) - 512)  # subtract the black level
+    im = np.maximum(im - 512, 0) / ((2 ** bps - 1) - 512)  # subtract the black level
 
     im = np.expand_dims(im, axis=2)
     img_shape = im.shape
@@ -24,9 +23,10 @@ def pack_raw(raw,bps=14):
                           im[1:H:2, 0:W:2, :]), axis=2)
     return out
 
+
 class LocalFilesUnetDataset(Dataset):
-    def __init__(self, images_list_file_name, count = 100 , bps=14, patch_size=256):
-        f=open(images_list_file_name)
+    def __init__(self, images_list_file_name, count=100, bps=14, patch_size=256):
+        f = open(images_list_file_name)
         gt_path = "dataset/Sony/long"
         in_path = "dataset/Sony/short"
         self.in_images = []
@@ -40,18 +40,18 @@ class LocalFilesUnetDataset(Dataset):
 
             in_name = os.path.basename(in_name)
             gt_name = os.path.basename(gt_name)
-            ratio = float(gt_name[9:-5])/float(in_name[9:-5])
+            ratio = float(gt_name[9:-5]) / float(in_name[9:-5])
 
-            in_name = os.path.join(in_path,in_name)
-            gt_name = os.path.join(gt_path,gt_name)
+            in_name = os.path.join(in_path, in_name)
+            gt_name = os.path.join(gt_path, gt_name)
 
-            self.in_images.append(pack_raw(rawpy.imread(in_name),bps)*ratio)
+            self.in_images.append(pack_raw(rawpy.imread(in_name), bps) * ratio)
 
             gt_raw = rawpy.imread(gt_name)
             im = gt_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
             self.gt_images.append(np.float32(im / 65535.0))
 
-            i+=1
+            i += 1
 
         f.close()
 
@@ -88,4 +88,3 @@ class LocalFilesUnetDataset(Dataset):
             gt_patch = np.transpose(gt_patch, (1, 0, 2)).copy()
 
         return npArrToTensor(input_patch), npArrToTensor(gt_patch)
-
